67a5ab137972298fb573a614a5d0a98a60649441
stephan <m6ahenina@gmail.com>
2021-05-04 13:11:23 +0300

    Sidebar menu with knp menu, material icons, roboto, translations
    - Sidebar menu is now built with knp menu bundle
    - Translation files, "users" translation domain
    - Material icons is working now
    Usage : 
        <span class="material-icons">add</span>
    - Roboto is now used as default font

--- assets/js/adminlte.js
+++ assets/js/adminlte.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/js/bootstrap';
 import 'overlayscrollbars/js/jquery.overlayScrollbars.js';
 import 'admin-lte/dist/js/adminlte.js';
-import '../scss/adminlte.scss';
 
 $(function () {
     $('.sidebar').overlayScrollbars({
