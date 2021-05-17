43ad1e6efe8e3e5c5ba31ef08171d3da6f8d4fc3
stephan <m6ahenina@gmail.com>
2021-05-17 09:37:28 +0300

    Init prospect manager
    - create Client entity (prospect)
    - create Address entity
    - create crud for client
    - add usefull trait in address and client (timestampable, blameable, softdeleteable)

---
+++ src/Form/ClientType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('shortName')
+            ->add('clientNumber')
+            ->add('activity')
+            ->add('tvaRate')
+            ->add('siret')
+            ->add('paymentMethod')
+            ->add('payment')
+            ->add('intraCommunityTva')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
