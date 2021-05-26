b42dc6706037fa1c47d3a386e9198f9d9c460237
stephan <m6ahenina@gmail.com>
2021-05-26 12:31:17 +0300

    WIP Project pdf template disegn

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Snappy\Pdf;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
@@ -18,10 +19,19 @@ class ProjectController extends AbstractController
     }
 
     #[Route('/pdf', name: 'project.pdf')]
-    public function pdf(Pdf $knpSnappyPdf)
+    public function pdf(Request $request, Pdf $knpSnappyPdf)
     {
-        $template = $this->renderView('project/pdf/index.html.twig'); 
-        
-        return new PdfResponse($knpSnappyPdf->getOutputFromHtml($template));
+        $template = $this->renderView('project/pdf/index.html.twig');
+        $options = [
+            'margin-left' => '2mm',
+            'margin-right' => '2mm',
+            'margin-top' => '0mm',
+        ];
+
+        if ($request->query->get('preview')) {
+            return new Response($template);
+        }
+
+        return new PdfResponse($knpSnappyPdf->getOutputFromHtml($template, $options));
     }
 }
