6d7565f17b44147d4b17e0e21ae645c834bb8b00
stephan <m6ahenina@gmail.com>
2021-05-31 13:35:12 +0300

    WIP almost ready to work with angular view

--- src/Entity/Constants/Project/MarketType.php
+++ src/Entity/Constants/Project/MarketType.php
@@ -14,7 +14,12 @@ class MarketType
     const SIMPLE_SUPPLY = 'simple_supply'; // Fourniture simple
     const ASBESTOS_REMOVAL = 'asbestos_removal'; // Désamiantage
 
-    const MARKE_TYPES_CHOICES = [
+    const PUBLIC_MARKET = 'public_market'; // marche publc
+    const PRIVATE_MARKET = 'private_market'; // marche privé
+    const PUBLIC_AO = 'public_ao'; // A.O public
+    const PRIVATE_AO = 'private_ao'; // A.O privé
+
+    const MARKET_TYPES_CHOICES = [
         self::WORK_ON_EXISTING => self::WORK_ON_EXISTING,
         self::TCE => self::TCE,
         self::NEW_BUILDING => self::NEW_BUILDING,
@@ -25,15 +30,11 @@ class MarketType
         self::SIMPLE_SUPPLY => self::SIMPLE_SUPPLY,
         self::ASBESTOS_REMOVAL => self::ASBESTOS_REMOVAL,
     ];
-    // const MARKE_TYPES_CHOICES = [
-    //     self::WORK_ON_EXISTING => self::WORK_ON_EXISTING,
-    //     self::TCE => self::TCE,
-    //     self::NEW_BUILDING => self::NEW_BUILDING,
-    //     self::AGRICULTURAL_BUILDING => self::AGRICULTURAL_BUILDING,
-    //     self::SHADES => self::SHADES,
-    //     self::CALL_FOR_TENDER => self::CALL_FOR_TENDER,
-    //     self::ISOTHERMAL_PLAN => self::ISOTHERMAL_PLAN,
-    //     self::SIMPLE_SUPPLY => self::SIMPLE_SUPPLY,
-    //     self::ASBESTOS_REMOVAL => self::ASBESTOS_REMOVAL,
-    // ];
+
+    const MARKET_TYPE_2_CHOICES = [
+        self::PUBLIC_MARKET => self::PUBLIC_MARKET,
+        self::PRIVATE_MARKET => self::PRIVATE_MARKET,
+        self::PUBLIC_AO => self::PUBLIC_AO,
+        self::PRIVATE_AO => self::PRIVATE_AO,
+    ];
 }
