82b3cd5e02f2c6f464aade13496dbf4524e5de05
patrick <rapaelec@gmail.com>
2021-06-01 09:46:18 +0300

    add project new front in process

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -7,8 +7,8 @@ use App\Form\ProjectType;
 use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Snappy\Pdf;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
