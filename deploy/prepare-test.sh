5754e4ab25febcdf6b63c5f788cab974da7bfdc4
patrick <rapaelec@gmail.com>
2021-06-16 22:04:22 +0300

    allow to write var directory

--- deploy/prepare-test.sh
+++ deploy/prepare-test.sh
@@ -6,6 +6,8 @@ composer install
 npm install
 yarn add maildev -g
 yarn encore dev
+chmod 777 -R var/cache
+chmod 777 -R var/log
 node_modules/maildev/bin/maildev --web 1080 --smtp 25 --hide-extensions STARTTLS &
 $PHP bin/console doctrine:database:drop --if-exists --env=test
 $PHP bin/console doctrine:database:create --if-not-exists --env=test
