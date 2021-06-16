b160d925d01545a0618b352e93196dd652b7c754
patrick <rapaelec@gmail.com>
2021-06-16 18:42:21 +0300

    add fixture

--- deploy/deploy-test.sh
+++ deploy/deploy-test.sh
@@ -7,7 +7,7 @@ yarn encore dev
 $PHP bin/console doctrine:database:drop --if-exists --env=test
 $PHP bin/console doctrine:database:create --if-not-exists --env=test
 $PHP bin/console doctrine:schema:update --force --env=test
-$PHP bin/console doctrine:fixtures:load --force --env=test
+$PHP bin/console doctrine:fixtures:load --no-interaction --env=test
 $PHP bin/console assets:install
 
 # Add write permissions on media folder
