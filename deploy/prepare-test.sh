a5949e42f540027a12d4911b2818420f80c2de2e
RAKOTOARIVELO <rapaelec@gmail.com>
2021-06-18 10:22:29 +0000

    Update prepare-test.sh

--- deploy/prepare-test.sh
+++ deploy/prepare-test.sh
@@ -5,7 +5,7 @@ echo "======= prepare for the test ========"
 chmod 777 -R var/cache
 chmod 777 -R var/log
 node_modules/maildev/bin/maildev --web 1080 --smtp 25 --hide-extensions STARTTLS &
-$PHP bin/console doctrine:database:drop --if-exists --env=test
+$PHP bin/console doctrine:database:drop --force --if-exists --env=test
 $PHP bin/console doctrine:database:create --if-not-exists --env=test
 $PHP bin/console doctrine:schema:update --force --env=test
 $PHP bin/console doctrine:fixtures:load --no-interaction --env=test
